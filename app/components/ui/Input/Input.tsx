a159d98b2f5cce5540ea68f2686ed498ad817336
Nikita Shalimo <shpoisnik@gmail.com>
2023-01-21 03:00:07 +0300

    add autocomplete places list

--- app/components/ui/Input/Input.tsx
+++ app/components/ui/Input/Input.tsx
@@ -2,6 +2,7 @@ import {FC, useEffect, useRef, useState} from 'react'
 import PlacesAutocomplete from 'react-places-autocomplete'
 import styles from './Input.module.scss'
 import {HiOutlineSearch} from 'react-icons/hi'
+import Spinner from '../Spinner/Spinner'
 
 interface IInput {
     choosedPlace: () => void,
@@ -51,6 +52,31 @@ const Input:FC<IInput> = ({choosedPlace, type}) => {
                         ref: inputRef,
                         placeholder: isFrom ? 'From' : 'To'
                     })}/>
+
+                    <div className='hide-scroll-bar absolute w-[320px] ml-[-20px] mt-44 overflow-y-auto rounded-b-lg z-10' style={suggestions.length ? {height: '130px'} : {height: 0}}>
+                        {loading && <Spinner/>}
+                        
+                        {suggestions.map(suggestion => {
+                const className = suggestion.active
+                  ? 'suggestion-item--active'
+                  : 'suggestion-item';
+                // inline style for demonstration purpose
+                const style = suggestion.active
+                  ? { backgroundColor: '#fafafa', cursor: 'pointer', padding: '5px' }
+                  : { backgroundColor: '#ffffff', cursor: 'pointer', padding: '5px' };
+                return (
+                  // eslint-disable-next-line react/jsx-key
+                  <div
+                    {...getSuggestionItemProps(suggestion, {
+                      className,
+                      style,
+                    })}
+                  >
+                    <span>{suggestion.description}</span>
+                  </div>
+                );
+              })}
+                    </div>
                 </div>
             </div>
            )

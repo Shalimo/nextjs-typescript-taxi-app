de1d6f50a708c8a0be99d768a346b178177967eb
Nikita Shalimo <shpoisnik@gmail.com>
2023-01-23 07:17:53 +0300

    toggle for hide and show main interface

--- app/components/screens/home/home/Home.tsx
+++ app/components/screens/home/home/Home.tsx
@@ -5,13 +5,23 @@ import ToInput from "../toInput/ToInput"
 import Variants from "../variants/Variants"
 import OrderButton from "../OrderButton/OrderButton"
 import styles from './Home.module.scss'
+import { useRef, useState } from "react"
+import { BiHide } from 'react-icons/bi'
 
 const Home = () => {
+
+  const [hide, setHide] = useState(false)
+
+  const hideInterface = () => {
+    setHide(!hide)
+    console.log(hide)
+  }
 
   return (
     <Layout title="Order">
       <GoogleMap />
-      <div className={styles.formContainer}>
+      <BiHide cursor='pointer' onClick={hideInterface} className={styles.hideButton} />
+      <div className={styles.formContainer} style={hide ? { display: 'none' } : { display: 'flex' }}>
         <FromInput />
         <ToInput />
         <Variants />
